34cd5ef8203cdf230e56b059267f9631743feb21
Jorge Alfredo Pérez Olivares <epijorgeperez@gmail.com>
2024-08-26 12:24:22 -0600

    cambio pagina inicio

--- components/Header.tsx
+++ components/Header.tsx
@@ -19,23 +19,23 @@ export default function Header() {
       </div>
       <h1 className="sr-only">Supabase and Next.js Starter Template</h1>
       <p className="text-3xl lg:text-4xl !leading-tight mx-auto max-w-xl text-center">
-        The fastest way to build apps with{" "}
+        Prototipo de aplicación de Reporte de{" "}
         <a
           href="https://supabase.com/?utm_source=create-next-app&utm_medium=template&utm_term=nextjs"
           target="_blank"
           className="font-bold hover:underline"
           rel="noreferrer"
         >
-          Supabase
+          Incidentes
         </a>{" "}
-        and{" "}
+        y{" "}
         <a
           href="https://nextjs.org/"
           target="_blank"
           className="font-bold hover:underline"
           rel="noreferrer"
         >
-          Next.js
+          Colisiones
         </a>
       </p>
       <div className="w-full p-[1px] bg-gradient-to-r from-transparent via-foreground/10 to-transparent my-8" />
